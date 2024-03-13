478c83667c788b5261b5da309b437b2ba0af250d
alextaylor5 <117131300+alextaylor5@users.noreply.github.com>
2024-03-12 19:06:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log ("This is my first line of java script I have written")
+console.log ("This is the second line of java script I have written")
+
+function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var session = document.getElementById('session');
+
+    if(hrs >= 12){
+        session.innerHTML = 'PM';
+    }else{
+        session.innerHTML = 'AM';
+    }
+
+    if(hrs > 12){
+        hrs = hrs - 12;
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 10);
